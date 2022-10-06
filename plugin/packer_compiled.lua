d486ff17f262604c15f51c286b7843999fbbc5e8
cbrgm <chris@cbrgm.net>
2022-10-06 22:57:17 +0200

    update

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -75,7 +75,7 @@ time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ["nvim-lspconfig"] = {
-    config = { "\27LJ\2\n0\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\21lsp.languages_go\frequire\0" },
+    config = { "\27LJ\2\n.\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\19plugins.lsp.go\frequire\0" },
     loaded = true,
     path = "/Users/christian.bargmann/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
@@ -90,7 +90,7 @@ _G.packer_plugins = {
 time([[Defining packer_plugins]], false)
 -- Config for: nvim-lspconfig
 time([[Config for nvim-lspconfig]], true)
-try_loadstring("\27LJ\2\n0\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\21lsp.languages_go\frequire\0", "config", "nvim-lspconfig")
+try_loadstring("\27LJ\2\n.\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\19plugins.lsp.go\frequire\0", "config", "nvim-lspconfig")
 time([[Config for nvim-lspconfig]], false)
 
 _G._packer.inside_compile = false
