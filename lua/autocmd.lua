4d901f7f31a0b28cc77538dc9ad4199637e762ed
cbrgm <chris@cbrgm.net>
2022-10-09 00:40:55 +0200

    update

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -3,8 +3,8 @@ local api = vim.api
 --- Remove all trailing whitespace on save
 local TrimWhiteSpaceGrp = api.nvim_create_augroup("TrimWhiteSpaceGrp", { clear = true })
 api.nvim_create_autocmd("BufWritePre", {
-  command = [[:%s/\s\+$//e]],
-  group = TrimWhiteSpaceGrp,
+	command = [[:%s/\s\+$//e]],
+	group = TrimWhiteSpaceGrp,
 })
 
 -- don't auto comment new line
