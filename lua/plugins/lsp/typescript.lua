aeffe9c74346cefe7422d666ca33879fd4ba839b
cbrgm <chris@cbrgm.net>
2022-10-10 10:17:24 +0200

    update

--- lua/plugins/lsp/typescript.lua
+++ lua/plugins/lsp/typescript.lua
@@ -1,6 +1,6 @@
 local nvim_lsp = require("lspconfig")
 local fn = require("functions")
-local cmp = require("plugins.nvim-cmp")
+local cmp = require("plugins.nvim-cmp.init")
 
 nvim_lsp.tsserver.setup({
 	on_attach = fn.on_attach,
