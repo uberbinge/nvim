f2003f8857456e0755f5ea1450d1ffc29815f20c
cbrgm <chris@cbrgm.net>
2022-10-11 23:17:11 +0200

    update

--- lua/plugins/git/init.lua
+++ lua/plugins/git/init.lua
@@ -1,6 +1,106 @@
--- load git plugins individually
-require("lua.plugins.git.gitsigns")
-require("lua.plugins.git.neogit")
+local packer = require("packer")
+
+packer.use({
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup {
+			keymaps = {
+				-- Default keymap options
+				noremap = false
+			},
+			signs = {
+				add = {
+					hl = "GitSignsAdd",
+					text = "│",
+					numhl = "GitSignsAddNr",
+					linehl = "GitSignsAddLn"
+				},
+				change = {
+					hl = "GitSignsChange",
+					text = "│",
+					numhl = "GitSignsChangeNr",
+					linehl = "GitSignsChangeLn"
+				},
+				delete = {
+					hl = "GitSignsDelete",
+					text = "_",
+					numhl = "GitSignsDeleteNr",
+					linehl = "GitSignsDeleteLn"
+				},
+				topdelete = {
+					hl = "GitSignsDelete",
+					text = "‾",
+					numhl = "GitSignsDeleteNr",
+					linehl = "GitSignsDeleteLn"
+				},
+				changedelete = {
+					hl = "GitSignsChange",
+					text = "~",
+					numhl = "GitSignsChangeNr",
+					linehl = "GitSignsChangeLn"
+				}
+			},
+			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+			numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+			linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+			word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+			watch_gitdir = { interval = 1000, follow_files = true },
+			attach_to_untracked = true,
+			-- git-blame provides also the time in contrast to gitsigns
+			current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+			current_line_blame_formatter_opts = { relative_time = false },
+			sign_priority = 6,
+			update_debounce = 100,
+			status_formatter = nil, -- Use default
+			max_file_length = 40000,
+			preview_config = {
+				-- Options passed to nvim_open_win
+				border = "single",
+				style = "minimal",
+				relative = "cursor",
+				row = 0,
+				col = 1
+			},
+			diff_opts = { internal = true },
+			yadm = { enable = false }
+		}
+	end,
+})
+
+packer.use({
+	"TimUntersberger/neogit",
+	requires = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("neogit").setup {
+			disable_signs = false,
+			disable_context_highlighting = false,
+			disable_commit_confirmation = false,
+			-- customize displayed signs
+			signs = {
+				-- { CLOSED, OPENED }
+				section = { ">", "v" },
+				item = { ">", "v" },
+				hunk = { "", "" }
+			},
+			integrations = { diffview = true },
+			-- override/add mappings
+			mappings = {
+				-- modify status buffer mappings
+				status = {
+					-- Adds a mapping with "B" as key that does the "BranchPopup" command
+					-- ["B"] = "BranchPopup",
+					-- ["C"] = "CommitPopup",
+					-- ["P"] = "PullPopup",
+					-- ["S"] = "Stage",
+					-- ["D"] = "Discard",
+					-- Removes the default mapping of "s"
+					-- ["s"] = "",
+				}
+			}
+		}
+	end,
+	cmd = "Neogit",
+})
 
 local wk = require("which-key")
 local gitsigns = require("gitsigns")
