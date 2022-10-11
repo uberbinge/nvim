f82e18906dd019970a9b0c1902bf75e879ce1d33
cbrgm <chris@cbrgm.net>
2022-10-11 10:29:51 +0200

    move which-key bindings into setup function

--- lua/plugins/which-key/init.lua
+++ lua/plugins/which-key/init.lua
@@ -8,68 +8,58 @@ packer.use({
 		wk.setup()
 
 		-- global keymaps
+
+		local default_options = { silent = true }
+
+		-- Register all leader based mappings
 		wk.register({
+			["<Tab>"] = { "<cmd>e#<cr>", "Prev buffer" },
+			["t"] = { "<cmd>ToggleTerm<cr>", "Terminal" },
 			b = {
-				name = "buffer",
-				d = { "<cmd>bd<CR>", "delete", noremap = true },
+				name = "Buffers",
+				b = {
+					"<cmd>Telescope buffers<cr>",
+					"Find buffer",
+				},
+				D = {
+					"<cmd>%bd|e#|bd#<cr>",
+					"Close all but the current buffer",
+				},
+				d = { "<cmd>bd<CR>", "Close buffer" },
+				f = { "<cmd>Neotree action=focus reveal=true<CR>", "Find file in file explorer", noremap = true },
+
 			},
-		}, {
-			mode = "n",
-			prefix = "<leader>",
-		})
+			f = {
+				name = "Files",
+				b = { "<cmd>Telescope file_browser grouped=true<cr>", "File browser" },
+				f = { "<cmd>Telescope find_files<cr>", "Find File" },
+				g = { "<cmd>Telescope live_grep<cr>", "Find in File" },
+				p = { "<cmd>Neotree reveal toggle<cr>", "Toggle Filetree" },
+				r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+				s = { "<cmd>w<cr>", "Save Buffer" },
+			},
+			g = { "Git" },
+			m = {
+				name = "Misc",
+				d = { "<cmd>lua require('functions').toggle_diagnostics()<cr>", "Toggle Diagnostics" },
+				o = { "Options" },
+				p = { "<cmd>PackerSync --preview<cr>", "PackerSync" },
+			},
+			q = {
+				name = "Quickfix",
+				j = { "<cmd>cnext<cr>", "Next Quickfix Item" },
+				k = { "<cmd>cprevious<cr>", "Previous Quickfix Item" },
+				q = { "<cmd>lua require('functions').toggle_qf()<cr>", "Toggle quickfix list" },
+			},
+			s = {
+				name = "Search", -- optional group name
+				f = { "<cmd>Telescope find_files hidden=true<cr>", "Find File", noremap = true },
+				g = { "<cmd>Telescope live_grep<cr>", "Find in File", noremap = true },
+				o = { "<cmd>Telescope oldfiles<cr>", "Open Recent File", noremap = true },
+				r = { "<cmd>Telescope resume<cr>", "Resume last picker", noremap = true },
+				p = { "<cmd>Telescope pickers<cr>", "List all pickers", noremap = true },
+				b = { "<cmd>Telescope buffers<cr>", "List open buffers", noremap = true },
+			},
+		}, { prefix = "<leader>", mode = "n", default_options })
 	end,
 })
-
-local wk = require("which-key")
-local default_options = { silent = true }
-
--- Register all leader based mappings
-wk.register({
-	["<Tab>"] = { "<cmd>e#<cr>", "Prev buffer" },
-	["t"] = { "<cmd>ToggleTerm<cr>", "Terminal" },
-	b = {
-		name = "Buffers",
-		b = {
-			"<cmd>Telescope buffers<cr>",
-			"Find buffer",
-		},
-		D = {
-			"<cmd>%bd|e#|bd#<cr>",
-			"Close all but the current buffer",
-		},
-		d = { "<cmd>bd<CR>", "Close buffer" },
-		f = { "<cmd>Neotree action=focus reveal=true<CR>", "Find file in file explorer", noremap = true },
-
-	},
-	f = {
-		name = "Files",
-		b = { "<cmd>Telescope file_browser grouped=true<cr>", "File browser" },
-		f = { "<cmd>Telescope find_files<cr>", "Find File" },
-		g = { "<cmd>Telescope live_grep<cr>", "Find in File" },
-		p = { "<cmd>Neotree reveal toggle<cr>", "Toggle Filetree" },
-		r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
-		s = { "<cmd>w<cr>", "Save Buffer" },
-	},
-	g = { "Git" },
-	m = {
-		name = "Misc",
-		d = { "<cmd>lua require('functions').toggle_diagnostics()<cr>", "Toggle Diagnostics" },
-		o = { "Options" },
-		p = { "<cmd>PackerSync --preview<cr>", "PackerSync" },
-	},
-	q = {
-		name = "Quickfix",
-		j = { "<cmd>cnext<cr>", "Next Quickfix Item" },
-		k = { "<cmd>cprevious<cr>", "Previous Quickfix Item" },
-		q = { "<cmd>lua require('functions').toggle_qf()<cr>", "Toggle quickfix list" },
-	},
-	s = {
-		name = "Search", -- optional group name
-		f = { "<cmd>Telescope find_files hidden=true<cr>", "Find File", noremap = true },
-		g = { "<cmd>Telescope live_grep<cr>", "Find in File", noremap = true },
-		o = { "<cmd>Telescope oldfiles<cr>", "Open Recent File", noremap = true },
-		r = { "<cmd>Telescope resume<cr>", "Resume last picker", noremap = true },
-		p = { "<cmd>Telescope pickers<cr>", "List all pickers", noremap = true },
-		b = { "<cmd>Telescope buffers<cr>", "List open buffers", noremap = true },
-	},
-}, { prefix = "<leader>", mode = "n", default_options })
